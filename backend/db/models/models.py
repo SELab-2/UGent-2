2dea5c7499ecf5daeb515a194f24a96fd8e9c89f
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-02-22 23:06:42 +0100

    SqlAlchemy models POC

---
+++ backend/db/models/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from backend.routes.extentions import db
+from backend.domain.models.models import Lesgever, Vak
+
+
+class LesgeverModel(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    naam: Mapped[str] = mapped_column()
+    vakken: Mapped[list["VakModel"]] = relationship("VakModel", back_populates="lesgever")
+
+    def to_domain_model(self) -> Lesgever:
+        return Lesgever(
+            id=self.id,
+            naam=self.naam
+        )
+
+
+class VakModel(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    naam: Mapped[str] = mapped_column()
+    lesgever_id: Mapped[int] = mapped_column(db.ForeignKey('lesgever_model.id'))
+    lesgever: Mapped["LesgeverModel"] = relationship("LesgeverModel", back_populates="vakken")
+
+    def to_domain_model(self):
+        return Vak(
+            id=self.id,
+            naam=self.naam,
+            lesgever_id=self.lesgever_id
+        )
