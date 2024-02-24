e59032b2c60fa5b66f18a8c13e3c5d1e164ae618
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-02-24 18:07:20 +0100

    Add domain models Closes #11

--- backend/domain/models/models.py
+++ backend/domain/models/models.py
@@ -1,23 +1,74 @@
 import dataclasses
 from abc import ABC
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass()
 class JsonRepresentable(ABC):
-
     def to_dict(self) -> dict:
         return dataclasses.asdict(self)
 
 
 @dataclass()
-class SubjectDataclass(JsonRepresentable):
+class UserDataclass(JsonRepresentable):
     name: str
-    teacher_id: int = None
-    id: int = None
+    email: str
+    id: int | None = None
+
+
+@dataclass()
+class AdminDataclass(JsonRepresentable):
+    id: int
 
 
 @dataclass()
 class TeacherDataclass(JsonRepresentable):
+    id: int
+    subject_ids: list[int] | None = None
+
+
+@dataclass()
+class StudentDataclass(JsonRepresentable):
+    id: int
+    subject_ids: list[int] | None = None
+    group_ids: list[int] | None = None
+    submission_ids: list[int] | None = None
+
+
+@dataclass()
+class SubjectDataclass(JsonRepresentable):
     name: str
-    id: int = None
+    id: int | None = None
+    teacher_ids: list[int] | None = None
+
+
+@dataclass()
+class ProjectDataclass(JsonRepresentable):
+    name: str
+    deadline: datetime
+    archived: bool
+    requirements: str
+    visible: bool
+    max_students: int
+    subject_id: int
+    id: int | None = None
+    groups: list[int] | None = None
+
+
+@dataclass()
+class GroupDataclass(JsonRepresentable):
+    project_id: int
+    id: int | None = None
+    student_ids: list[int] | None = None
+    submission_ids: list[int] | None = None
+
+
+@dataclass()
+class SubmissionDataclass(JsonRepresentable):
+    date_time: datetime
+    group_id: int
+    student_id: int
+    state: int
+    message: str
+    id: int | None = None
