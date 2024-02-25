9633432136a4df4ebaf2392b93d1d34f64132584
ALBERICLOOS <albericloos2003@gmail.com>
2024-02-25 13:33:52 +0100

    toevoegen uniqueconstraint error

--- backend/db/errors/database_errors.py
+++ backend/db/errors/database_errors.py
@@ -1,3 +1,8 @@
 class ItemNotFoundError(Exception):
     def __init__(self, message: str):
         super().__init__(message)
+
+
+class UniqueConstraintError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
