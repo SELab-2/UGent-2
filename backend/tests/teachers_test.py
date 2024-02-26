62f6bb851b584858349370ebaf99e36cfeb46d74
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-02-26 14:13:47 +0100

    ruff fix: PT009 use assert instead of unittest assert

--- backend/tests/teachers_test.py
+++ backend/tests/teachers_test.py
@@ -12,13 +12,13 @@ class LesgeverTestCase(unittest.TestCase):
 
     def test_create_teacher_bad_request(self):
         response = self.app.post("/teachers", data=json.dumps({}), content_type="application/json")
-        self.assertEqual(HTTPStatus.BAD_REQUEST, response.status_code)
-        self.assertIn("error", json.loads(response.data))
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+        assert "error" in json.loads(response.data)
 
     def test_create_teacher_success(self):
         teacher_data = {"name": "Bart De Bruyn"}
         response = self.app.post("/teachers", data=json.dumps(teacher_data), content_type="application/json")
-        self.assertEqual(HTTPStatus.CREATED, response.status_code)
+        assert response.status_code == HTTPStatus.CREATED
 
 
 if __name__ == "__main__":
