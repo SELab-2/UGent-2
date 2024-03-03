63ec82d569aa135d4636a2f5b3f2d981c550fbd1
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-03-03 16:10:32 +0100

    Add projects routes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
+from routes.projects import projects_router
 from routes.subjects import subjects_router
 from routes.users import users_router
 
@@ -9,6 +10,7 @@ app = FastAPI()
 # Koppel routes uit andere modules.
 app.include_router(subjects_router, prefix="/api")
 app.include_router(users_router, prefix="/api")
+app.include_router(projects_router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run("app:app")
---
+++ backend/routes/projects.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, HTTPException
+
+from db.errors.database_errors import ItemNotFoundError
+from domain.models.ProjectDataclass import ProjectDataclass
+from routes.db import get_dao_provider
+from routes.login import get_authenticated_user
+
+projects_router = APIRouter()
+
+
+@projects_router.get("/projects")
+def get_subjects(teacher: bool = False) -> list[ProjectDataclass]:
+    user = get_authenticated_user()
+    project_dao = get_dao_provider().get_project_dao()
+    subject_dao = get_dao_provider().get_subject_dao()
+    try:
+        if teacher:
+            subjects = subject_dao.get_subjects_of_teacher(user.id)
+        else:
+            subjects = subject_dao.get_subjects_of_student(user.id)
+        projects = []
+        for i in subjects:
+            projects += project_dao.get_projects_of_subject(i.id)
+    except ItemNotFoundError as err:
+        raise HTTPException(status_code=404) from err
+    return projects
+
+
+@projects_router.get("/projects/{subject_id}")
+def get_project(project_id: int) -> ProjectDataclass:
+    project_dao = get_dao_provider().get_project_dao()
+    subject_dao = get_dao_provider().get_subject_dao()
+    user = get_authenticated_user()
+    try:
+        project = project_dao.get(project_id)
+    except ItemNotFoundError as err:
+        raise HTTPException(status_code=404) from err
+    subject = subject_dao.get(project.subject_id)
+    if not subject.is_user_authorized(user, get_dao_provider()):
+        raise HTTPException(status_code=403)
+    return project
