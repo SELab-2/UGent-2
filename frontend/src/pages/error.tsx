ff371f1d71a0a8af548f96d3271b183fbe8a7d1d
Matthias Seghers <matthias.h.seghers@gmail.com>
2024-03-03 15:20:26 +0100

    removed linting error in unkown type of error in error.tsx

--- frontend/src/pages/error.tsx
+++ frontend/src/pages/error.tsx
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom";
+import {isRouteErrorResponse, useRouteError} from "react-router-dom";
 import {JSX} from 'react';
 
 export default function ErrorPage(): JSX.Element {
@@ -10,8 +10,29 @@ export default function ErrorPage(): JSX.Element {
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
             <p>
-                <i>{error.statusText || error.message}</i>
+                <i>{errorMessage(error)}</i>
             </p>
         </div>
     );
+}
+
+interface RouterError extends Error {}
+
+function isRouterError(object: unknown): object is RouterError {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    return 'message' in object;
+}
+
+function errorMessage(error: unknown): string {
+    if (isRouteErrorResponse(error)) {
+        return `${error.status} ${error.statusText}`
+    } else if (error != undefined && isRouterError(error)) {
+        return error.message;
+    } else if (typeof error === 'string') {
+        return error
+    } else {
+        console.error(error)
+        return 'Unknown error'
+    }
 }
