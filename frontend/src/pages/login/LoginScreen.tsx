5337fcfdbc6b550ceaebad7e31468d2c842a71fd
EmmaVandewalle <emmatje.vandewalle@gmail.com>
2024-03-07 13:07:52 +0100

    fix: clean login-screen

--- frontend/src/pages/login/LoginScreen.tsx
+++ frontend/src/pages/login/LoginScreen.tsx
@@ -1,21 +1,7 @@
 import {JSX} from "react";
-import {Header} from "../../components/Header.tsx";
-import {Sidebar} from "../../components/Sidebar.tsx";
 
 export default function LoginScreen(): JSX.Element {
     return (
-        <>
-            <div className={"main-header"}>
-                <Header/>
-            </div>
-            <div className={"main-content is-flex is-flex-direction-row"}>
-                <div className={"side-bar"}>
-                    <Sidebar/>
-                </div>
-                <div>
-                    <>Login screen</>
-                </div>
-            </div>
-        </>
+        <>Login screen</>
     )
 }
