5e89504c368a4c773d4f29f9c4cbb25c6559c587
EmmaVandewalle <emmatje.vandewalle@gmail.com>
2024-03-07 12:58:20 +0100

    fix: width of logo

--- frontend/src/pages/login/LoginScreen.tsx
+++ frontend/src/pages/login/LoginScreen.tsx
@@ -1,7 +1,21 @@
 import {JSX} from "react";
+import {Header} from "../../components/Header.tsx";
+import {Sidebar} from "../../components/Sidebar.tsx";
 
 export default function LoginScreen(): JSX.Element {
     return (
-        <>Login screen</>
+        <>
+            <div className={"main-header"}>
+                <Header/>
+            </div>
+            <div className={"main-content is-flex is-flex-direction-row"}>
+                <div className={"side-bar"}>
+                    <Sidebar/>
+                </div>
+                <div>
+                    <>Login screen</>
+                </div>
+            </div>
+        </>
     )
 }
