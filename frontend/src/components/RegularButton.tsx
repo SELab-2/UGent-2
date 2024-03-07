c101a6fea28fab5aeac6036392a56d9af6334cab
ALBERICLOOS <albericloos2003@gmail.com>
2024-03-07 14:17:36 +0100

    change import

--- frontend/src/components/RegularButton.tsx
+++ frontend/src/components/RegularButton.tsx
@@ -1,12 +1,13 @@
 import {JSX} from "react";
-import { IoMdAdd } from "react-icons/io";
-export function RegularButton(props: {placeholder: string, add: boolean}): JSX.Element {
+import {IoMdAdd} from "react-icons/io";
+
+export function RegularButton(props: { placeholder: string, add: boolean }): JSX.Element {
     return (
         <button className="button is-rounded">
             {props.add ? (
                 <span className="icon">
-          <IoMdAdd />
-        </span>
+                    <IoMdAdd/>
+                </span>
             ) : null}
             <span>{props.placeholder}</span>
         </button>
