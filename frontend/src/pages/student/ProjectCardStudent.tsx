8d9a714c7737df0d39456cae0fb1b254767dbe59
EmmaVandewalle <emmatje.vandewalle@gmail.com>
2024-03-09 16:37:31 +0100

    fix: layout cards and important force removed

--- frontend/src/pages/student/ProjectCardStudent.tsx
+++ frontend/src/pages/student/ProjectCardStudent.tsx
@@ -4,8 +4,8 @@ import {JSX} from "react";
 export default function ProjectCardStudent(): JSX.Element {
     // as for now I'm not sure how the info will come in, so using
     // static values for now (change to true and true/false to see how they'll look like):
-    let has_submission: boolean = false;
-    let isSuccess: boolean = false;
+    let has_submission: boolean = true;
+    let isSuccess: boolean = true;
 
     return (
         <div
@@ -18,8 +18,10 @@ export default function ProjectCardStudent(): JSX.Element {
             <div className={"is-flex is-flex-direction-row is-align-items-center py-5"}>
                 <p className={"pr-2"}>status: </p>
                 {!has_submission && <p className={"px-2 py-1"}>—</p>}
-                {has_submission && isSuccess && <p className={"card p-positive px-2 py-1"}>Success</p>}
-                {has_submission && !isSuccess && <p className={"card p-negative px-2 py-1"}>Failed</p>}
+                {has_submission && isSuccess &&
+                    <div className={"card"}><p className={"p-positive px-2 py-1"}>Success</p></div>}
+                {has_submission && !isSuccess &&
+                    <div className={"card"}><p className={"p-negative px-2 py-1"}>Failed</p></div>}
             </div>
         </div>
     )
