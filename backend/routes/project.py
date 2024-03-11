cfcaf4af233b59c9aa4f3a8d17ccdc1a03c548fc
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-03-11 14:45:32 +0100

    Add route for joining subject

--- backend/routes/project.py
+++ backend/routes/project.py
@@ -6,10 +6,7 @@ from domain.logic.group import get_groups_of_project
 from domain.logic.project import get_project
 from domain.models.GroupDataclass import GroupDataclass
 from domain.models.ProjectDataclass import ProjectDataclass
-from routes.dependencies.role_dependencies import (
-    ensure_user_authorized_for_project,
-    get_authenticated_user,
-)
+from routes.dependencies.role_dependencies import ensure_user_authorized_for_project
 
 project_router = APIRouter()
 
@@ -18,7 +15,6 @@ project_router = APIRouter()
 def project_get(
     project_id: int,
     session: Session = Depends(get_session),
-    uid: int = Depends(get_authenticated_user),
 ) -> ProjectDataclass:
     project: ProjectDataclass = get_project(session, project_id)
     return project
@@ -28,6 +24,5 @@ def project_get(
 def project_get_groups(
     project_id: int,
     session: Session = Depends(get_session),
-    uid: int = Depends(get_authenticated_user),
 ) -> list[GroupDataclass]:
     return get_groups_of_project(session, project_id)
--- backend/routes/student.py
+++ backend/routes/student.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from db.sessions import get_session
 from domain.logic.project import get_projects_of_student
-from domain.logic.subject import get_subjects_of_student
+from domain.logic.subject import add_student_to_subject, get_subjects_of_student
 from domain.models.ProjectDataclass import ProjectDataclass
 from domain.models.StudentDataclass import StudentDataclass
 from domain.models.SubjectDataclass import SubjectDataclass
@@ -26,3 +26,12 @@ def projects_of_student_get(
     student: StudentDataclass = Depends(get_authenticated_student),
 ) -> list[ProjectDataclass]:
     return get_projects_of_student(session, student.id)
+
+
+@student_router.post("/student/subjects{subject_id}/join")
+def student_subject_join(
+    subject_id: int,
+    session: Session = Depends(get_session),
+    student: StudentDataclass = Depends(get_authenticated_student),
+) -> None:
+    add_student_to_subject(session, student.id, subject_id)
