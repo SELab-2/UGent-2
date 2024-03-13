a24b201675980f4186f35206f962835c082118ee
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-03-13 16:12:57 +0100

    remove main function #58

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,14 +1,6 @@
-# main.py
-import unittest
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 DB_TEST_URI = "postgresql://postgres:postgres@localhost:5432/delphi-test"
 test_engine = create_engine(DB_TEST_URI)
 SessionLocal = sessionmaker(autocommit=False, bind=test_engine)
-
-
-if __name__ == "__main__":
-    test_suite = unittest.TestLoader().discover(".")
-    unittest.TextTestRunner().run(test_suite)
