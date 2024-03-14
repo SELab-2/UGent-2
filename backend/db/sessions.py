ef765464f228003fd3ae517b6a3dc2c9c7880f54
Ruben Vandamme <ruben.vandamme@ugent.be>
2024-03-14 10:46:52 +0100

    code annotations db

--- backend/db/sessions.py
+++ backend/db/sessions.py
@@ -4,10 +4,15 @@ from sqlalchemy.orm import Session, sessionmaker
 
 from db.extensions import engine
 
+# Generator for db sessions. Check the [documentation.md] for the use of sessions.
 SessionLocal: sessionmaker[Session] = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def get_session() -> Generator[Session, None, None]:
+    """
+    Returns a generator for session objects.
+    To be used as dependency injection.
+    """
     db = SessionLocal()
     try:
         yield db
