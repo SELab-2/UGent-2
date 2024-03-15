a51cf9eb44812fc3884865e4a01a594ec9a8a70e
EmmaVandewalle <emmatje.vandewalle@gmail.com>
2024-03-15 21:21:34 +0100

    feat: button to close settings in popup

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -2,12 +2,16 @@ import {BiLogOut} from "react-icons/bi";
 import '../assets/styles/small_components.css'
 import {JSX} from "react";
 import {Link} from "react-router-dom";
+import { IoMdClose } from "react-icons/io";
 
 
-export default function Settings(): JSX.Element {
+export default function Settings(props: {closeSettings: () => void}): JSX.Element {
     return (
-        <>
-            <div className={"card popup px-5 py-5"}>
+        <div className={"card popup"}>
+            <div className={"is-flex is-align-items-center is-justify-content-right"}>
+                <button className={"px-1 py-1 mx-1 my-1"} onClick={props.closeSettings}><IoMdClose/></button>
+            </div>
+            <div className={"px-5 pb-5"}>
                 <p className={"title is-flex is-justify-content-center"}>Settings</p>
                 <img src={"/delphi_full.png"} alt={"image"}/>
                 <div className={"is-flex px-5 py-1 is-align-items-center"}>
@@ -26,6 +30,6 @@ export default function Settings(): JSX.Element {
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
