0fca6e35e8451e043cfba219c0439c61a5064df1
EmmaVandewalle <emmatje.vandewalle@gmail.com>
2024-03-15 21:23:23 +0100

    feat: logo.png added and in use

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ import {RiAccountPinBoxLine} from "react-icons/ri";
 export function Header(): JSX.Element {
     return (
         <nav className={"main-nav is-flex is-flex-direction-row is-justify-content-space-between is-align-items-center"}>
-            <img src={"/delphi_favicon_circle.png"} alt={"image"}/>
+            <img src={"/logo.png"} alt={"image"}/>
             <p className={"is-align-self-center"}><p>Home</p></p>
             <button className={"button mx-2 is-transparent"}>
                 <div className={"icon-text"}>
