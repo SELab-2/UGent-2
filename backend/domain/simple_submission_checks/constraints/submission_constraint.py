8b436c4bad93de5e154f1faa5f6633955693167c
lbarraga <lukas.barragantorres@ugent.be>
2024-03-17 21:04:02 +0100

    pyright :/ #43

--- backend/domain/simple_submission_checks/constraints/submission_constraint.py
+++ backend/domain/simple_submission_checks/constraints/submission_constraint.py
@@ -10,5 +10,5 @@ from domain.simple_submission_checks.validation_result import ValidationResult
 class SubmissionConstraint(BaseModel):
     root_constraint: ZipConstraint | FileConstraint
 
-    def validate(self, path: Path) -> ValidationResult:
+    def validate_constraint(self, path: Path) -> ValidationResult:
         return self.root_constraint.validate_constraint(path)
