5ec5077ae600f0efcc15ed7d489e3e273977b78a
lbarraga <lukas.barragantorres@ugent.be>
2024-03-17 20:41:13 +0100

    Simpele zip constraint #43

---
+++ backend/domain/simple_submission_checks/constraints/zip_constraint.py
@@ -0,0 +1,36 @@
+import os
+import tempfile
+import zipfile
+from typing import Literal
+
+from pydantic import BaseModel
+
+from domain.simple_submission_checks.constraints.directory_constraint import DirectoryConstraint
+from domain.simple_submission_checks.constraints.file_constraint import FileConstraint
+from domain.simple_submission_checks.constraints.not_present_constraint import NotPresentConstraint
+from domain.simple_submission_checks.validation_result import ErrorResult, OkResult
+
+
+class ZipConstraint(BaseModel):
+    type: Literal["zip_constraint"]
+    name: str
+    sub_constraints: list[DirectoryConstraint | FileConstraint | NotPresentConstraint]
+
+    def validate_constraint(self, path: str):
+        directory = os.listdir(path)
+
+        # Check if file is present.
+        if self.name not in directory:
+            return ErrorResult(f"Zip file '{self.name}' not present.")
+
+        # Check if file is a zip file.
+        zip_path = os.path.join(path, self.name)
+        if not zipfile.is_zipfile(zip_path):
+            return ErrorResult(f"File '{self.name}' is not a zip file.")
+
+        # Extract file into a Temp directory and validate sub constraints.
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                zip_ref.extractall(tmp_dir)
+                sub_constraints = [constraint.validate_constraint(tmp_dir) for constraint in self.sub_constraints]
+                return OkResult(f"Zip file '{self.name}' present.", sub_constraints)
