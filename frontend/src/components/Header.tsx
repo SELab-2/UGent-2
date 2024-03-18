0b47ab275d8c50ab7a0ca4b3f7e8b85d8ddcf765
ALBERICLOOS <albericloos2003@gmail.com>
2024-03-18 14:41:52 +0100

    Add variable page title

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import {JSX} from "react";
 import {RiAccountPinBoxLine} from "react-icons/ri";
 
-export function Header(): JSX.Element {
+export function Header(props: { page_title: string }): JSX.Element {
     return (
-        <nav className={"main-nav is-flex is-flex-direction-row is-justify-content-space-between is-align-items-center"}>
+        <nav
+            className={"main-nav is-flex is-flex-direction-row is-justify-content-space-between is-align-items-center"}>
             <img src={"/delphi_favicon_circle.png"} alt={"image"}/>
-            <p className={"is-align-self-center"}><p>Home</p></p>
+            <p className={"is-align-self-center"}><p>{props.page_title}</p></p>
             <button className={"button mx-2 is-transparent"}>
                 <div className={"icon-text"}>
                     <p>Username</p>
