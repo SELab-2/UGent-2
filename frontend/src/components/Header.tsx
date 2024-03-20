20a2204e42c12569d6c3106703a600d359f9b895
EmmaVandewalle <emmatje.vandewalle@gmail.com>
2024-03-20 14:26:31 +0100

    fix: logo doesn't resize weirdly anymore

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,9 +6,11 @@ export function Header(): JSX.Element {
     return (
         <nav
             className={"main-nav is-flex is-flex-direction-row is-justify-content-space-between is-align-items-center"}>
-            <Link to={"/"} className={"main-nav"}>
-                <img src={"/logo.png"} alt={"image"}/>
-            </Link>
+            <div className={"logo-div is-flex is-align-items-center"}>
+                <Link to={"/"}>
+                    <img src={"/logo.png"} alt={"image"}/>
+                </Link>
+            </div>
             <p className={"is-align-self-center"}><p>Home</p></p>
             <button className={"button mx-2 is-transparent"}>
                 <div className={"icon-text"}>
