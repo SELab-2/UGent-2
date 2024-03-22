ee4842a5ebd4f566726423ffa312a11f2d12778b
Stef Ossé <stef.osse@ugent.be>
2024-03-22 14:56:51 +0100

    merge conflicts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,45 +12,12 @@ import HomeAdmin from "./pages/admin/HomeAdmin.tsx";
 import HomeStudent from "./pages/student/HomeStudent.tsx";
 import HomeTeacher from "./pages/teacher/HomeTeacher.tsx";
 import ErrorPage from "./pages/error.tsx";
-import studentLoader from "./dataloaders/StudentLoader.ts";
+import studentLoader, {STUDENT_ROUTER_ID} from "./dataloaders/StudentLoader.ts";
 import Unauthorized from "./components/authentication/Unauthorized.tsx";
+import teacherLoader, {TEACHER_ROUTER_ID} from "./dataloaders/TeacherLoader.ts";
+import SubjectsTeacher from "./pages/teacher/SubjectsTeacher.tsx";
+import subjectsTeacherLoader, {SUBJECT_TEACHER_ROUTER_ID} from "./dataloaders/SubjectsTeacherLoader.ts";
 
-/* TODO: merge main --> routes + correct arguments
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root/>,
-        errorElement: <ErrorPage/>
-    },
-    {
-        path: "/login",
-        element: <LoginScreen/>
-    },
-    {
-        path: "/admin",
-        element: <HomeAdmin/>
-    },
-    {
-        path: "/student",
-        element: <HomeStudent/>,
-        id: STUDENT_ROUTER_ID,
-        loader: studentLoader
-    },
-    {
-        path: "/teacher",
-        element: <HomeTeacher/>,
-        id: TEACHER_ROUTER_ID,
-        loader: teacherLoader
-    },
-    {
-        path: "/teacher/courses",
-        element: <SubjectsTeacher/>,
-        id: SUBJECT_TEACHER_ROUTER_ID,
-        loader: subjectsTeacherLoader
-    }
-]);
-*/
-        
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
@@ -62,7 +29,7 @@ createRoot(document.getElementById('root')!).render(
 
                     {/* Protected routes */}
                     <Route element={<RequireAuth allowedRoles={['ADMIN', 'STUDENT', 'TEACHER']}/>}>
-                        <Route path={"/"} element={<Root/>}/>
+                        <Route path={"/"} element={<Root/>} errorElement={<ErrorPage/>}/>
                     </Route>
 
                     <Route element={<RequireAuth allowedRoles={['ADMIN']}/>}>
@@ -70,11 +37,13 @@ createRoot(document.getElementById('root')!).render(
                     </Route>
 
                     <Route element={<RequireAuth allowedRoles={['STUDENT']}/>}>
-                        <Route path={"student"} element={<HomeStudent/>} loader={studentLoader}/>
+                        <Route id={STUDENT_ROUTER_ID} path={"student"} element={<HomeStudent/>} loader={studentLoader}/>
                     </Route>
 
                     <Route element={<RequireAuth allowedRoles={['TEACHER']}/>}>
-                        <Route path={"teacher"} element={<HomeTeacher/>}/>
+                        <Route id={TEACHER_ROUTER_ID} path={"teacher"} element={<HomeTeacher/>} loader={teacherLoader}/>
+                        <Route path={"/teacher/courses"} element={<SubjectsTeacher/>} id={SUBJECT_TEACHER_ROUTER_ID}
+                               loader={subjectsTeacherLoader}/>
                     </Route>
 
                     <Route path={"*"} element={<ErrorPage/>}/>
