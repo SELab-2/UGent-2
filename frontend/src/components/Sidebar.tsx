1fe49b87ec9076712ab45c58951bddc47b4faedb
Ruben Vandamme <ruben.vandamme@ugent.be>
2024-03-26 18:14:07 +0100

    specific sidebar buttons and config changes

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -3,23 +3,34 @@ import { PiProjectorScreen, PiFolder } from "react-icons/pi";
 import { IoMdSettings } from "react-icons/io";
 import Settings from "./Settings.tsx";
 
-export function Sidebar(): JSX.Element {
+export enum SidebarButton {
+    COURSES,
+    PROJECTS
+}
+
+
+export function Sidebar(params: {buttons?: SidebarButton[]}): JSX.Element {
     const [isOpen, setIsOpen] = useState(false);
 
     const handleSettings: () => void = () => {
         setIsOpen(!isOpen)
     }
 
+    var buttons = params.buttons
+    if (buttons == undefined) {
+        buttons = [SidebarButton.COURSES, SidebarButton.PROJECTS];
+    }
+    
     return (
         <>
             <aside className={"menu is-flex is-flex-direction-column is-justify-content-space-between"}>
                 <ul className={"menu-list"}>
-                    <li>
-                        <a><PiProjectorScreen/></a>
-                    </li>
-                    <li>
-                        <a><PiFolder/></a>
-                    </li>
+                    {SidebarButton.COURSES in buttons &&
+                        (<li><a><PiProjectorScreen/></a></li>)
+                    }
+                    {SidebarButton.PROJECTS in buttons &&
+                        (<li><a><PiFolder/></a></li>)
+                    }
                 </ul>
                 <ul className={"menu-list"}>
                     <li>
--- frontend/src/pages/admin/HomeAdmin.tsx
+++ frontend/src/pages/admin/HomeAdmin.tsx
@@ -1,7 +1,21 @@
 import {JSX} from "react";
+import {Header} from "../../components/Header.tsx";
+import {Sidebar} from "../../components/Sidebar.tsx";
 
 export default function HomeAdmin(): JSX.Element {
     return (
-        <>Homescreen for an admin</>
+        <>
+            <div className={"main-header"}>
+                <Header/>
+            </div>
+            <div className={"main-content is-flex is-flex-direction-row"}>
+                <div className={"side-bar is-flex is-justify-content-center"}>
+                    <Sidebar buttons={[]}/>
+                </div>
+                <div className={"admin-main is-flex"}>
+                    <>Test</>
+                </div>
+            </div>
+        </>
     )
 }
