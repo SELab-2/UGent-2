0352abad9bd52009fc5462281cf47692ad005243
EmmaVandewalle <emmatje.vandewalle@gmail.com>
2024-03-28 13:36:19 +0100

    fix: no component for small div needed

--- frontend/src/pages/student/CoursesViewStudent.tsx
+++ frontend/src/pages/student/CoursesViewStudent.tsx
@@ -2,9 +2,31 @@ import {JSX} from "react";
 import {Header} from "../../components/Header.tsx";
 import {Sidebar} from "../../components/Sidebar.tsx";
 import '../../assets/styles/students_components.css'
-import ViewCoursesStudentComponent from "../../components/ViewCoursesStudentComponent.tsx";
+import {SearchBar} from "../../components/SearchBar.tsx";
+import {Table, TableRowCourses} from "../../components/Table.tsx";
 
 export default function CoursesViewStudent(): JSX.Element {
+
+    const TableDataCoursesActive: TableRowCourses[] = [
+        {
+            name: "Automaten, berekenbaarheid & complexiteit",
+            numberOfProjects: 1,
+            shortestDeadline: "17:00 - 23/02/2024"
+        },
+        {
+            name: "Computationele Biologie",
+            numberOfProjects: 2,
+            shortestDeadline: "19:00 - 25/02/2024"
+        },
+    ];
+    const TableCoursesArchived: TableRowCourses[] = [
+        {
+            name: "Logisch Programmeren",
+            numberOfProjects: 1,
+            shortestDeadline: null,
+        }
+    ];
+
     return (
         <>
             <div className={"main-header"}>
@@ -15,7 +37,11 @@ export default function CoursesViewStudent(): JSX.Element {
                     <Sidebar/>
                 </div>
                 <div className={"student-main is-flex is-justify-content-center"}>
-                    <ViewCoursesStudentComponent/>
+                    <div className={"table-page is-flex is-flex-direction-column"}>
+                        <SearchBar placeholder={"zoek een vak..."}/>
+                        <Table title={"actief"} data={TableDataCoursesActive}/>
+                        <Table title={"gearchiveerd"} data={TableCoursesArchived}/>
+                    </div>
                 </div>
             </div>
         </>
