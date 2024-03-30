918f24b1ab39f4234d65d89ad9f7bd4df202abbd
ALBERICLOOS <albericloos2003@gmail.com>
2024-03-30 17:59:32 +0100

    more react style

--- frontend/src/components/Statistics.tsx
+++ frontend/src/components/Statistics.tsx
@@ -1,4 +1,4 @@
-import {JSX} from "react";
+import {JSX, useState} from "react";
 import {PieChart} from "react-minimal-pie-chart";
 import {IoIosStats} from "react-icons/io";
 
@@ -9,57 +9,30 @@ export default function Statistics(): JSX.Element {
         {title: 'Nothing yet', value: 20, color: '#D3D3D3'},
     ]
 
-    // default functie voor bulma modal gehaald van
-    document.addEventListener('DOMContentLoaded', () => {
-        // Functions to open and close a modal
-        function openModal($el: HTMLElement | null) {
-            if ($el === null) {
-                return;
-            }
-            $el.classList.add('is-active');
-        }
+    const [modalActive, setModalActive] = useState(false);
 
-        function closeModal($el: Element | null) {
-            if ($el === null) {
-                return;
-            }
-            $el.classList.remove('is-active');
-        }
+    const openModal = () => {
+        setModalActive(true);
+    };
 
-        // Add a click event on buttons to open a specific modal
-        (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-            const modal = "modal-stats"
-            const $target = document.getElementById(modal);
-
-            $trigger.addEventListener('click', () => {
-                openModal($target);
-            });
-        });
-
-        // Add a click event on various child elements to close the parent modal
-        (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-            const $target = $close.closest('.modal');
-
-            $close.addEventListener('click', () => {
-                closeModal($target);
-            });
-        });
-    });
+    const closeModal = () => {
+        setModalActive(false);
+    };
 
     return (
         <>
-            <button className="js-modal-trigger button is-primary is-pulled-right" data-target="modal-stats">
+            <button className="js-modal-trigger button is-primary is-pulled-right" onClick={openModal}>
                 <span className="icon is-small">
                     <IoIosStats/>
                 </span>
                 <span>Statistieken</span>
             </button>
-            <div id="modal-stats" className="modal">
-                <div className="modal-background"></div>
+            <div id="modal-stats" className={`modal ${modalActive ? 'is-active' : ''}`}>
+                <div className="modal-background" onClick={closeModal}></div>
                 <div className="modal-card">
                     <header className="modal-card-head">
                         <p className="modal-card-title">Indieningen:</p>
-                        <button className="delete" aria-label="close"></button>
+                        <button className="delete" aria-label="close" onClick={closeModal}></button>
                     </header>
                     <section className="modal-card-body">
                         <PieChart
