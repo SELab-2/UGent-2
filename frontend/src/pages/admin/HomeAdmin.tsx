95925715ada802094b24b2d8b0fb589197416e18
Ruben Vandamme <ruben.vandamme@ugent.be>
2024-03-30 13:57:16 +0100

    werkend inklap-/uitklapmodel

--- frontend/src/pages/admin/HomeAdmin.tsx
+++ frontend/src/pages/admin/HomeAdmin.tsx
@@ -1,6 +1,6 @@
 import {JSX} from "react";
 import '../../assets/styles/simple_checks.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { SelectionBox } from "../../components/SelectionBox";
 import { TbArrowRightBar } from "react-icons/tb";
 
@@ -82,34 +82,148 @@ export default function HomeAdmin(): JSX.Element {
             }
         ]
     }
-    
-    function parse(structure: Constraint): JSX.Element[] {
-        let lines: JSX.Element[] = []
-        if (structure.sub_constraints) {
-            parseSubs(structure.sub_constraints, lines)
-        }
-        return lines;
+
+    enum Type {
+        ZIP,
+        DIRECTORY,
+        FILE
     }
 
-    function parseSubs(structures: Constraint[], lines: JSX.Element[], before: string = "") {
+    interface CheckObject {
+        id: number,
+        parent_id: number | undefined,
+        name: string, 
+        shown: boolean, 
+        spaces: number, 
+        last: boolean, 
+        type: Type
+    }
+
+    const [checkObjects, setCheckObjects] = useState<CheckObject[]>(initialLoad())
+
+    function initialLoad(): CheckObject[] {
+        const list = []
+        // first main constraint
+        if (test_structure.type == "file_constraint") {
+            // file-constraint
+            list.push({
+                id: 0, 
+                parent_id: undefined,
+                name: test_structure.name, 
+                shown: true, 
+                spaces: 0, 
+                last: true, 
+                type: Type.FILE
+            })
+        } else {    
+            // zip-constraint
+            list.push({
+                id: 0, 
+                parent_id: undefined,
+                name: test_structure.name, 
+                shown: true, 
+                spaces: 0, 
+                last: true, 
+                type: Type.ZIP
+            })
+
+            if (test_structure.sub_constraints) {
+                parseRecursive(list, 0, 1, test_structure.sub_constraints)
+            }
+        }
+        return list
+    }
+
+    // sub constraints
+    function parseRecursive(list: CheckObject[], parent_id: number, spaces: number, sub_structures: Constraint[]): number {
         let i = 0;
-        for (let structure of structures) {
-            if (i < structures.length - 1) {
-                if (structure.sub_constraints) {
-                    lines.push(<div>{before + "├─\u00A0" + structure.name}<button>+</button></div>)
-                    parseSubs(structure.sub_constraints, lines, before + "\u00A0|\u00A0\u00A0\u00A0\u00A0")
+        let id = parent_id + 1;
+        for (let structure of sub_structures) {
+            if (i < sub_structures.length - 1) {
+                // niet de laatste
+                if (structure.type == "directory_constraint") {
+                    // een directory
+                    list.push({
+                        id: id,
+                        parent_id: parent_id,
+                        name: structure.name, 
+                        shown: false, 
+                        spaces: spaces, 
+                        last: false, 
+                        type: Type.DIRECTORY
+                    })
+                    if (structure.sub_constraints) {
+                        id = parseRecursive(list, id, spaces+1, structure.sub_constraints)
+                    }
                 } else {
-                    lines.push(<div>{before + "├─\u00A0" + structure.name}</div>)
+                    // een file
+                    list.push({
+                        id: id, 
+                        parent_id: parent_id,
+                        name: structure.name, 
+                        shown: false, 
+                        spaces: spaces, 
+                        last: false, 
+                        type: Type.FILE
+                    })
                 }
             } else {
-                if (structure.sub_constraints) {
-                    lines.push(<div>{before + "└─\u00A0" + structure.name}<button>+</button></div>)
-                    parseSubs(structure.sub_constraints, lines, before + "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0")
+                // wel de laatste
+                if (structure.type == "directory_constraint") {
+                    // een directory
+                    list.push({
+                        id: id, 
+                        parent_id: parent_id,
+                        name: structure.name, 
+                        shown: false, 
+                        spaces: spaces, 
+                        last: true, 
+                        type: Type.DIRECTORY
+                    })
+                    if (structure.sub_constraints) {
+                        id = parseRecursive(list, id, spaces+1, structure.sub_constraints)
+                    }
                 } else {
-                    lines.push(<div>{before + "└─\u00A0" + structure.name}</div>)
+                    // een file
+                    list.push({
+                        id: id, 
+                        parent_id: parent_id,
+                        name: structure.name, 
+                        shown: false, 
+                        spaces: spaces, 
+                        last: true, 
+                        type: Type.FILE
+                    })
                 }
             }
             i++;
+            id++;
+        }
+        return id - 1; // don't count last increase
+    }
+
+    function changeVisibility(id: number): undefined {
+        let closing = checkObjects.filter(checkObject => checkObject.parent_id === id && !checkObject.shown).length === 0
+        if (!closing) {
+            let newCheckObjects = []
+            for (let checkObject of checkObjects) {
+                if (checkObject.parent_id === id) {
+                    checkObject.shown = true
+                }
+                newCheckObjects.push(checkObject)
+            }
+            setCheckObjects(newCheckObjects)
+        } else {
+            let ids = [id]
+            let newCheckObjects = []
+            for (let checkObject of checkObjects) {
+                if (checkObject.parent_id !== undefined && ids.includes(checkObject.parent_id)) {
+                    checkObject.shown = false
+                    ids.push(checkObject.id)
+                }
+                newCheckObjects.push(checkObject)
+            }
+            setCheckObjects(newCheckObjects)
         }
     }
 
@@ -136,8 +250,18 @@ export default function HomeAdmin(): JSX.Element {
                 </div>
                 <p className="specify">Specifieer welke files de zip moet bevatten:</p>
                 <div className="recursive">
-                    {parse(test_structure).map(
-                        line => line
+                    {checkObjects.map(
+                        checkObject => 
+                        <div> {checkObject.shown &&
+                            <div className="line">
+                                <div>{"\u00A0".repeat(6 * checkObject.spaces)}</div>
+                                <div className="check-object">
+                                    <p>{checkObject.id}</p>
+                                    {(checkObject.type == Type.ZIP || checkObject.type == Type.DIRECTORY) &&
+                                        <button onClick={() => changeVisibility(checkObject.id)}>x</button>}
+                                </div>
+                            </div>
+                        }</div>
                     )}
                 </div>
                 <label className="checkbox">
