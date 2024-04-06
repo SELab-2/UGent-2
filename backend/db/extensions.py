fddf2394f1ce6b7df57b38b1a1dd30064f42d7b2
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-04-06 21:50:34 +0200

    comment refactor + sqlmodel import #123

--- backend/db/extensions.py
+++ backend/db/extensions.py
@@ -1,23 +1,18 @@
 import os
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlmodel import create_engine
 
 """
 Retrieve the variables needed for a connection to the database.
 We use environment variables for the code to be more adaptable across different environments.
 The second variable in os.getenv specifies the default value.
 """
-# where the database is hosted
-db_host = os.getenv("DB_HOST", "localhost")
-# port number on which the database server is listening
-db_port = os.getenv("DB_PORT", "5432")
-# username for the database
-db_user = os.getenv("DB_USERNAME", "postgres")
-# password for the user
-db_password = os.getenv("DB_PASSWORD", "postgres")
-# name of the database
-db_database = os.getenv("DB_DATABASE", "delphi")
+
+db_host = os.getenv("DB_HOST", "localhost")         # where the database is hosted
+db_port = os.getenv("DB_PORT", "5432")              # port number on which the database server is listening
+db_user = os.getenv("DB_USERNAME", "postgres")      # username for the database
+db_password = os.getenv("DB_PASSWORD", "postgres")  # password for the user
+db_database = os.getenv("DB_DATABASE", "delphi")    # name of the database
 
 # dialect+driver://username:password@host:port/database
 DB_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_database}"
@@ -25,10 +20,3 @@ DB_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_database
 # The engine manages database-operations.
 # There is only one instance of the engine, specified here.
 engine = create_engine(DB_URI)
-
-
-class Base(DeclarativeBase):
-    """
-    This class is meant to be inherited from to define the database tables, see [db/models/models.py].
-    For usage, please check https://docs.sqlalchemy.org/en/20/orm/declarative_styles.html#using-a-declarative-base-class.
-    """
