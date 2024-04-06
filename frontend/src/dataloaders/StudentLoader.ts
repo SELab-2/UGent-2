d8651936e6e8b82d7d090ed1d0b235702f68343f
ALBERICLOOS <albericloos2003@gmail.com>
2024-04-06 19:19:53 +0200

    move project dataloader for student to other file

--- frontend/src/dataloaders/StudentLoader.ts
+++ frontend/src/dataloaders/StudentLoader.ts
@@ -1,5 +1,5 @@
 import {CompleteProject} from "../utils/ApiInterfaces.ts";
-import {projectsLoader, teacherStudentRole} from "./SharedFunctions.ts";
+import {projectsLoaderStudent} from "./ProjectsStudentLoader.ts";
 
 export interface studentLoaderObject {
     projects: CompleteProject[]
@@ -9,6 +9,6 @@ export const STUDENT_ROUTER_ID = "student";
 
 
 export default async function studentLoader(): Promise<studentLoaderObject> {
-    const projects: CompleteProject[] = await projectsLoader(teacherStudentRole.STUDENT);
+    const projects: CompleteProject[] = await projectsLoaderStudent();
     return {projects};
 }
