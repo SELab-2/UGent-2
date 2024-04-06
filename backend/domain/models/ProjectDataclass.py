cd0cb291c9b0d14c14c4e797a2fb5ebf0d00b538
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-04-06 21:55:47 +0200

    removed all dataclasses except APIUser #123

--- backend/domain/models/ProjectDataclass.py
+++ backend/domain/models/ProjectDataclass.py
@@ -3,18 +3,6 @@ from datetime import datetime
 from pydantic import BaseModel, PositiveInt
 
 
-class ProjectDataclass(BaseModel):
-    id: int
-    name: str
-    deadline: datetime
-    archived: bool
-    description: str
-    requirements: str
-    visible: bool
-    max_students: PositiveInt
-    subject_id: int
-
-
 class ProjectInput(BaseModel):
     name: str
     deadline: datetime
