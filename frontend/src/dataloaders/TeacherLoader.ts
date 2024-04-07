107b9970cdd0a07b8f7fb36ba1c9905f879497e2
ALBERICLOOS <albericloos2003@gmail.com>
2024-04-07 13:18:58 +0200

    linting

--- frontend/src/dataloaders/TeacherLoader.ts
+++ frontend/src/dataloaders/TeacherLoader.ts
@@ -1,14 +1,14 @@
-import {CompleteProject} from "../utils/ApiInterfaces.ts";
+import {CompleteProjectTeacer} from "../utils/ApiInterfaces.ts";
 import {LoadProjectsForTeacher} from "./projectsTeacherLoader.ts";
 
 export interface teacherLoaderObject {
-    projects: CompleteProject[]
+    projects: CompleteProjectTeacer[]
 }
 
 export const TEACHER_ROUTER_ID = "teacher";
 
 export default async function teacherLoader(): Promise<teacherLoaderObject> {
-    const projects: CompleteProject[] = (await LoadProjectsForTeacher())
+    const projects: CompleteProjectTeacer[] = (await LoadProjectsForTeacher())
         .filter(project => !project.project_archived && project.project_visible);
     return {projects};
 }
