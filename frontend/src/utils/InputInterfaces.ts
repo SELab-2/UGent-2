6669ee69b0f7c3493d0b5b117eb7b016237f1eb5
Stef Ossé <stef.osse@ugent.be>
2024-04-09 21:39:56 +0200

    Functies user, projects, subjects, group, course

---
+++ frontend/src/utils/InputInterfaces.ts
@@ -0,0 +1,9 @@
+export interface ProjectInput{
+    name: string,
+    deadline: Date,
+    archived: boolean,
+    description: string,
+    requirements: string,
+    visible: boolean,
+    max_students: number,
+}
