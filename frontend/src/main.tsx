e4f138425553bde3f1b4a3931d8b6d962567639d
Ruben Vandamme <vandamme_r@outlook.com>
2024-04-12 16:00:11 +0200

    i18n project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -44,7 +44,7 @@ const router = createBrowserRouter(
         <Route path={"/"}>
 
 
-            <Route path={"test"} element={<ProjectsViewTeacher/>}/>
+            <Route path={"test"} element={<ProjectViewTeacher/>}/>
 
 
             {/* Public routes */}
