413ce8d984cb763571a308fbe0b915b342da0e60
ALBERICLOOS <albericloos2003@gmail.com>
2024-04-13 11:39:15 +0200

    add real data to page

--- frontend/src/pages/teacher/CreateProject.tsx
+++ frontend/src/pages/teacher/CreateProject.tsx
@@ -4,18 +4,33 @@ import {Header} from "../../components/Header.tsx";
 import {Sidebar} from "../../components/Sidebar.tsx";
 import {ProjectTeacher} from "../../types/project.ts";
 import {RegularButton} from "../../components/RegularButton.tsx";
+import {useRouteLoaderData} from "react-router-dom";
+import {coursesTeacherLoaderObject, CREATE_PROJECT_TEACHER_ID} from "../../dataloaders/CoursesTeacherLoader.ts";
 
 export function CreateProject(): JSX.Element {
+
+    const data = useRouteLoaderData(CREATE_PROJECT_TEACHER_ID) as coursesTeacherLoaderObject;
+    console.log(data.courses);
+
+    if (data.courses.length === 0) {
+        return <>
+            there was an error loading the courses
+        </>
+    }
+
+    // het enige dat we hier moeten laden zijn alle courses van de teacher
     const emptyProjectTeacher: ProjectTeacher = {
-        courseName: "",
+        courseName: data.courses[0].subject_name,
+        all_courses: data.courses,
         deadline: new Date(Date.now()),
         description: "",
         groupProject: false,
-        hours: 22,
-        minutes: 58,
+        hours: 0,
+        minutes: 0,
         otherFilesAllow: false,
         projectName: "",
         requiredFiles: "",
+        maxGroupMembers: 0,
     };
 
     return (
@@ -29,7 +44,8 @@ export function CreateProject(): JSX.Element {
                 </div>
                 <div className={"student-main my-3 is-flex is-flex-direction-column"}>
                     <div className={"mx-5"}>
-                        <RegularButton placeholder={"Save"} add={false} onClick={() => {}}/>
+                        <RegularButton placeholder={"Save"} add={false} onClick={() => {
+                        }}/>
                     </div>
                     <ProjectTeacherComponent project={emptyProjectTeacher}/>
                 </div>
