af2cf82159ffc24b5da6c1d3225a572b7be8722c
Matthias Seghers <matthias.h.seghers@gmail.com>
2024-04-16 14:06:54 +0200

    split in different roles on request of emma

--- frontend/src/dataloaders/AdminLoader.ts
+++ frontend/src/dataloaders/AdminLoader.ts
@@ -7,11 +7,20 @@ import {User} from "../utils/ApiInterfaces.ts";
 export const ADMIN_LOADER = "ADMIN_LOADER";
 
 export interface AdminLoaderObject {
-    users: User[];
+    students: User[]
+    teachers: User[]
+    admins: User[]
+}
+
+function filter_on_role(users: User[], role: string): User[] {
+    return users.filter((user) => user.user_roles.includes(role));
 }
 
 export default async function adminLoader(): Promise<AdminLoaderObject> {
     const users_backend: Backend_user[] = (await apiFetch("/api/users")) as Backend_user[];
     const users: User[] = users_backend.map((user) => mapUser(user));
-    return {users};
+    const students = filter_on_role(users, "STUDENT");
+    const teachers = filter_on_role(users, "TEACHER");
+    const admins = filter_on_role(users, "ADMIN");
+    return {students, teachers, admins};
 }
