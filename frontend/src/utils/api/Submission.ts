0025e406447da8829f516058b52076856c6a75aa
Stef Ossé <stef.osse@ugent.be>
2024-04-18 16:07:52 +0200

    changes pr

--- frontend/src/utils/api/Submission.ts
+++ frontend/src/utils/api/Submission.ts
@@ -1,15 +1,15 @@
+/* TODO: When this function is implemented --> so that type file is known
 export async function make_submission(groupId: number, file: File) {
-    /* TODO: When this function is implemented --> so that type file is known
-    const submissionData = (await apiFetch<Backend_submission>(`/groups/${groupId}/submission`,
-        {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            },
-            method: 'POST',
-            body: JSON.stringify({file: file})
-        }));
-    if (submissionData.ok){
-        return mapSubmission(submissionData.content)
-    }
-     */
+const submissionData = (await apiFetch<Backend_submission>(`/groups/${groupId}/submission`,
+    {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+        method: 'POST',
+        body: JSON.stringify({file: file})
+    }));
+if (submissionData.ok){
+    return mapSubmission(submissionData.content)
 }
+}
+ */
