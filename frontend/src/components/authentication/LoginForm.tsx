b224c46cdadb718c098a711547b079c5f76dc04f
EmmaVandewalle <emmatje.vandewalle@gmail.com>
2024-04-25 21:08:07 +0200

    fix: change css so .content is gone to overwrite everything with boxes and center

--- frontend/src/components/authentication/LoginForm.tsx
+++ frontend/src/components/authentication/LoginForm.tsx
@@ -1,24 +1,21 @@
 import {JSX} from "react";
 import {DEBUG} from "../../pages/root.tsx";
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
 export default function LoginForm(): JSX.Element {
     let url = 'https://login.ugent.be/login?service=https://sel2-2.ugent.be/login'
-    if (DEBUG){
+    if (DEBUG) {
         url = "https://login.ugent.be/login?service=https://localhost:8080/login"
     }
 
-    const { t } = useTranslation();
+    const {t} = useTranslation();
 
     return (
-        <div className="centered-card">
-            <div className="card m-6 p-6 content has-text-centered">
-                <img src={"/logo.png"} alt="Delphi logo"/>
-                <h1 className="title width"> {t('login.title')}</h1>
-                <p className="subtitle">{t('login.text')}</p>
-                <a className="button is-primary is-large" href={url}>{t('login.button')}</a>
-            </div>
+        <div className="card m-6 p-6 content has-text-centered">
+            <img src={"/logo.png"} alt="Delphi logo"/>
+            <h1 className="title width"> {t('login.title')}</h1>
+            <p className="subtitle">{t('login.text')}</p>
+            <a className="button is-primary is-large" href={url}>{t('login.button')}</a>
         </div>
     )
-        ;
 }
