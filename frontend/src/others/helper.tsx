9cb0b00ad7b106cc6b2c14012edfc54b1065041e
Ruben Vandamme <ruben.vandamme@ugent.be>
2024-04-27 22:29:31 +0200

    moved keys

--- frontend/src/others/helper.tsx
+++ frontend/src/others/helper.tsx
@@ -9,12 +9,12 @@ export default function RenderProjectCards(props: { projects: Project[] | Comple
 
             {props.projects.map((project) => {
                 if ('course_id' in project && props.courses) {
-                    return <Link to={`/teacher/project/${project.project_id}`}>
-                        <ProjectCardTeacher key={project.project_id} project={project} course={props.courses.filter(e => e.course_id == project.course_id)[0]}/>
+                    return <Link to={`/teacher/project/${project.project_id}`} key={project.project_id}>
+                        <ProjectCardTeacher project={project} course={props.courses.filter(e => e.course_id == project.course_id)[0]}/>
                     </Link>;
                 } else if ("submission_state" in project) {
-                    return <Link to={`/student/project/${project.project_id}`}>
-                        <ProjectCardStudent key={project.project_id} project={project} />
+                    return <Link to={`/student/project/${project.project_id}`} key={project.project_id}>
+                        <ProjectCardStudent project={project} />
                     </Link>;
                 }
             })}
