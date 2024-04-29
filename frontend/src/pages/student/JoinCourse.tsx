23a7cabd76d6ba3436936506c2c4ef4748abbba0
Matthias Seghers <matthias.h.seghers@gmail.com>
2024-04-29 16:51:41 +0200

    should work?

--- frontend/src/pages/student/JoinCourse.tsx
+++ frontend/src/pages/student/JoinCourse.tsx
@@ -1,26 +1,39 @@
 import {JSX} from "react";
 import {useTranslation} from "react-i18next";
-import {Link} from "react-router-dom";
+import {Link, useRouteLoaderData} from "react-router-dom";
 import ApiFetch from "../../utils/ApiFetch.ts";
+import {JOIN_COURSE, JoinCourseObject} from "../../dataloaders/JoinCourse.ts";
 
 export default function JoinCourseScreen(): JSX.Element {
 
-    const course_id = 1;
-    const course_name = "Course 1";
     const {t} = useTranslation();
+    const data = useRouteLoaderData(JOIN_COURSE) as JoinCourseObject;
+    if (data.error) {
+        return joinCourseError(data.error);
+    }
     return (
 
         <div>
-            <h1>{`${t('join_course.message')} ${course_name}?`}</h1>
-            <a onClick={() => joinCourse(course_id)}>{t('join_course.join')}</a>
+            <h1>{`${t('join_course.message')} ${data.course_name}?`}</h1>
+            <a onClick={() => {
+                joinCourse(data.course_id);
+                window.location.replace(`/student/course/${data.course_id}`)
+            }}>{t('join_course.join')}</a>
             <Link to={"/student"}>{t('join_course.return_home')}</Link>
+        </div>
+    )
+}
+
+function joinCourseError(error: string): JSX.Element {
+    // TODO: Implement this
+    return (
+        <div>
+            <h1>{error}</h1>
         </div>
     )
 }
 
 function joinCourse(course_id: number) {
     // Do something
-    console.log(course_id)
-    void ApiFetch<void>(`/student/course/${course_id}/join`, {method: 'POST'});
-    window.location.href.replace(/\/join$/, '')
+    void ApiFetch<void>(`/student/courses/${course_id}/join`, {method: 'POST'});
 }
