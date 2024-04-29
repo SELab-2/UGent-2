f013e9a1d08f9fafa3f0dbd41e4f74c5dff3475b
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-04-29 09:04:50 +0200

    Run container on submission

--- backend/controllers/routes/submission.py
+++ backend/controllers/routes/submission.py
@@ -2,21 +2,35 @@ import datetime
 import hashlib
 import pathlib
 
-from fastapi import APIRouter, Response, UploadFile
+from fastapi import APIRouter, BackgroundTasks, Response, UploadFile
+from sqlmodel import Session
 from starlette.requests import Request
 
 from controllers.authentication.role_dependencies import ensure_student_in_group, ensure_user_authorized_for_submission
 from controllers.swagger_tags import Tags
+from db.extensions import engine
 from db.models import Submission, SubmissionState
+from domain.logic.docker import run_container
 from domain.logic.errors import InvalidSubmissionError
 from domain.logic.group import get_group
-from domain.logic.submission import check_submission, create_submission, get_last_submission
+from domain.logic.submission import check_submission, create_submission, get_last_submission, get_submission
 
 submission_router = APIRouter(tags=[Tags.SUBMISSION])
 
 
+def run_docker_checks(submission_id: int) -> None:
+    with Session(engine) as session:
+        submission = get_submission(session, submission_id)
+        res = run_container(submission.group.project.image_id)
+        if res:
+            submission.state = SubmissionState.Approved
+        else:
+            submission.state = SubmissionState.Rejected
+        session.commit()
+
+
 @submission_router.post("/groups/{group_id}/submission", summary="Make a submission.")
-def make_submission(request: Request, group_id: int, file: UploadFile) -> Submission:
+def make_submission(request: Request, group_id: int, file: UploadFile, tasks: BackgroundTasks) -> Submission:
     session = request.state.session
     student = ensure_student_in_group(request, group_id)
     if file.filename is None or ".." in file.filename or file.filename == ".":
@@ -36,7 +50,7 @@ def make_submission(request: Request, group_id: int, file: UploadFile) -> Submis
     project = get_group(session, group_id).project
     state = SubmissionState.Approved if project.image_id == "" else SubmissionState.Pending
 
-    return create_submission(
+    submission = create_submission(
         session=session,
         student_id=student.id,
         group_id=group_id,
@@ -45,6 +59,9 @@ def make_submission(request: Request, group_id: int, file: UploadFile) -> Submis
         date_time=datetime.datetime.now(),
         filename=filename,
     )
+    if state == SubmissionState.Pending:
+        tasks.add_task(run_docker_checks, submission.id)
+    return submission
 
 
 @submission_router.get("/groups/{group_id}/submission", summary="Get latest submission.")
--- backend/domain/logic/docker.py
+++ backend/domain/logic/docker.py
@@ -12,3 +12,9 @@ def build_image(dockerfile: str) -> str:
             f.write(dockerfile)
         image, _ = cast(tuple[Image, Any], client.images.build(path=tmpdir))
     return cast(str, image.id)
+
+
+def run_container(image_id: str) -> bool:
+    client = docker.from_env()
+    container = client.containers.run(image_id, detach=True, remove=True)
+    return container.wait() == 0
