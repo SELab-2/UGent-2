4ef5122d8e4232d8d915c4551355ad385b958e5d
EmmaVandewalle <emmatje.vandewalle@gmail.com>
2024-05-06 11:08:13 +0200

    feat: add layout

--- frontend/src/pages/student/JoinCourse.tsx
+++ frontend/src/pages/student/JoinCourse.tsx
@@ -1,30 +1,35 @@
 import {JSX} from "react";
 import {useTranslation} from "react-i18next";
 import {Link, useRouteLoaderData} from "react-router-dom";
-import {JOIN_COURSE, JoinCourseObject} from "../../dataloaders/JoinCourse.ts";
 import {join_course} from "../../utils/api/Student.ts";
+import {JOIN_COURSE, JoinCourseObject} from "../../dataloaders/JoinCourse.ts";
 
 export default function JoinCourseScreen(): JSX.Element {
-
     const {t} = useTranslation();
     const {course_id, course_name, error} = useRouteLoaderData(JOIN_COURSE) as JoinCourseObject;
+
     if (error) {
         return joinCourseError(error);
     }
+
     return (
-        <div>
-            <h1>{`${t('join_course.message')} ${course_name}?`}</h1>
-            <a onClick={() => {
-                join_course(course_id);
-                window.location.replace(`/student/course/${course_id}`)
-            }}>{t('join_course.join')}</a>
-            <Link to={"/student"}>{t('join_course.return_home')}</Link>
+        <div className={"join-course card has-text-centered"}>
+            <div className={"card p-6"}>
+                <p className={"title p-6"}>{`${t('join_course.message')} ${course_name}?`}</p>
+                <div className={"is-flex is-justify-content-space-evenly p-6"}>
+                    <a className={"button"} onClick={() => {
+                        join_course(course_id);
+                        window.location.replace(`/student/course/${course_id}`)
+                    }}>{t('join_course.join')}</a>
+                    <Link className={"button"} to={"/student"}>{t('join_course.return_home')}</Link>
+                </div>
+            </div>
         </div>
     )
 }
 
 function joinCourseError(error: string): JSX.Element {
-    // TODO: Implement this
+    // TODO: Implement this: redirect?
     return (
         <div>
             <h1>{error}</h1>
--- frontend/src/utils/api/Student.ts
+++ frontend/src/utils/api/Student.ts
@@ -1,12 +1,12 @@
 import apiFetch from "../ApiFetch.ts";
 
-export function join_course(courseId: number): void{
+export function join_course(courseId: number): void {
     void apiFetch(`/student/courses/${courseId}/join`, {
         method: 'POST'
-    });
+    }).catch(() => console.log("something went wrong"));
 }
 
-export function leave_course(courseId: number): void{
+export function leave_course(courseId: number): void {
     void apiFetch(`/student/courses/${courseId}/leave`, {
         method: 'POST'
     });
