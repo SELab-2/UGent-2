8e77732ba77b42e091669ca570593ef2ab1d6777
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-05-12 16:48:35 +0200

    Also rebuild image on project update

--- backend/domain/logic/docker.py
+++ backend/domain/logic/docker.py
@@ -4,6 +4,10 @@ from typing import Any, cast
 
 import docker
 from docker.models.images import Image
+from sqlmodel import Session
+
+from db.extensions import engine
+from domain.logic.project import get_project
 
 
 def build_image(dockerfile: str) -> str:
@@ -12,7 +16,15 @@ def build_image(dockerfile: str) -> str:
         with open(f"{tmpdir}/Dockerfile", "w") as f:
             f.write(dockerfile)
         image, _ = cast(tuple[Image, Any], client.images.build(path=tmpdir))
-    return cast(str, image.id)
+    return str(image.id)
+
+
+def add_image_id(project_id: int) -> None:
+    with Session(engine) as session:
+        project = get_project(session, project_id)
+        image_id = build_image(project.dockerfile)
+        project.image_id = image_id
+        session.commit()
 
 
 def run_container(image_id: str, submission: bytes) -> tuple[str, bool]:
