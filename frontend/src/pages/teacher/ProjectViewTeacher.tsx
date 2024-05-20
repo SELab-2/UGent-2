c9d05a8a48c157cea013ad6be73acce7dd92ddeb
Ruben Vandamme <ruben.vandamme@ugent.be>
2024-05-20 12:17:02 +0200

    adjusted scrollbars

--- frontend/src/pages/teacher/ProjectViewTeacher.tsx
+++ frontend/src/pages/teacher/ProjectViewTeacher.tsx
@@ -2,8 +2,6 @@ import {Header} from "../../components/Header.tsx";
 import {Sidebar} from "../../components/Sidebar.tsx";
 import {ProjectTeacherComponent} from "../../components/ProjectTeacherComponent.tsx";
 import {ProjectTeacher} from "../../types/project.ts";
-import Statistics from "../../components/Statistics.tsx";
-import {RegularButton} from "../../components/RegularButton.tsx";
 import {useRouteLoaderData} from "react-router-dom";
 import {PROJECT_TEACHER, ProjectTeacherLoaderObject} from "../../dataloaders/ProjectTeacher.ts";
 import {useTranslation} from 'react-i18next';
@@ -46,13 +44,8 @@ export default function ProjectViewTeacher() {
                 <div className={"side-bar is-flex is-justify-content-center"}>
                     <Sidebar home={"teacher"}/>
                 </div>
-                <div className={"student-main my-3 is-flex is-flex-direction-column"}>
-                    <div className={"mx-5 mb-5 is-flex is-justify-content-start"}>
-                        <RegularButton placeholder={t('project.save')} add={false} onClick={() => {}}/>
-                        <div className={"mr-5"}/>
-                        <Statistics statistics={project_data.submission_statistics}/>
-                    </div>
-                    <ProjectTeacherComponent project={project}/>
+                <div className={"student-main is-flex is-flex-direction-column"}>
+                    <ProjectTeacherComponent project={project} submission_statistics={project_data.submission_statistics}/>
                 </div>
             </div>
         </>
