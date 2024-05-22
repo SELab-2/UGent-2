ed378538c687cfdd6a50401f3d198ff6eb71f709
Ruben Vandamme <ruben.vandamme@ugent.be>
2024-05-22 13:31:18 +0200

    remove debug variable & redesign download button

--- frontend/src/components/ProjectTeacherComponent.tsx
+++ frontend/src/components/ProjectTeacherComponent.tsx
@@ -172,10 +172,6 @@ export function ProjectTeacherComponent(props: {
                     </div>
                     <div className="field-body field">
                         <div className="field"> {/* Deze moet er blijven, anders doet css raar*/}
-                            {
-                                requiredFilesHasChanged && 
-                                <div>changed</div>
-                            }
                             <SimpleTests
                                 teacherOrStudent={TeacherOrStudent.TEACHER}
                                 initialData={requiredFiles}
