63b262920a32a99237d2fd9aeef9d679c9b1ed98
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-05-23 20:09:11 +0200

    change name of extension not present constraint

--- backend/domain/simple_submission_checks/constraints/extension_not_present_constraint.py
+++ backend/domain/simple_submission_checks/constraints/extension_not_present_constraint.py
@@ -11,22 +11,22 @@ from domain.simple_submission_checks.constraints.constraint_result import (
 
 class ExtensionNotPresentConstraint(BaseModel):
     type: ConstraintType = ConstraintType.EXTENSION_NOT_PRESENT
-    extension: str
+    not_present_extension: str
 
     def validate_constraint(self, path: Path) -> ExtensionNotPresentConstraintResult:
         directory = os.listdir(path)
 
-        files_with_extension = [file for file in directory if file.endswith(self.extension)]
+        files_with_extension = [file for file in directory if file.endswith(self.not_present_extension)]
 
         if files_with_extension:
             return ExtensionNotPresentConstraintResult(
-                extension=self.extension,
+                extension=self.not_present_extension,
                 is_ok=False,
                 files_with_extension=files_with_extension,
             )
 
         return ExtensionNotPresentConstraintResult(
-            extension=self.extension,
+            extension=self.not_present_extension,
             is_ok=True,
             files_with_extension=files_with_extension,
         )
