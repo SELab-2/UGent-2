061dd0c2284ac990aec076c9af2a8f08316bc53b
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-05-23 21:04:22 +0200

    Remove console.log

--- frontend/src/components/ManageCourse.tsx
+++ frontend/src/components/ManageCourse.tsx
@@ -8,7 +8,6 @@ import adminLoader from "../dataloaders/AdminLoader.ts";
 async function get_teachers(current_teachers: { name: string, email: string, id: number }[]) {
     const data = await adminLoader(); // Replace this with your actual data fetching logic
     const users = data.users.filter((user) => user.user_roles.includes("TEACHER"));
-    console.log(users)
     return users.map((user) => ({
         name: user.user_name,
         email: user.user_email,
